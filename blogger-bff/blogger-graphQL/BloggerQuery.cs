f42120a4cd3a4e8df83abc65615f7c8e49625085
Mr. AB <napster.ashish@gmail.com>
2018-12-02 19:59:27 +0000

    Added list-blogs functionality in blogbff

--- blogger-bff/blogger-graphQL/BloggerQuery.cs
+++ blogger-bff/blogger-graphQL/BloggerQuery.cs
@@ -10,11 +10,17 @@ namespace blogger_graphQL
         public BloggerQuery(BlogData data)
         {
             Name = "Query";
-            Field<BlogType>("blog", arguments: new QueryArguments(
-                    new QueryArgument<NonNullGraphType<StringGraphType>> { Name = "id", Description = "id of the blog" }
-                ),
-                resolve: context => data.GetBlogByIdAsync(context.GetArgument<string>("id"))
-                );
+
+            Field<ListGraphType<BlogType>>("blogs",
+                arguments: new QueryArguments(                    
+                    new QueryArgument<IntGraphType> { Name = "index", Description = "index of first entity." },
+                    new QueryArgument<IntGraphType> { Name = "count", Description = "size of batch" }                    
+                    ),
+                resolve: context => data.GetBlogsAsync(context.GetArgument<int>("index"), context.GetArgument<int>("count")));
+
+            Field<BlogType>("blog", 
+                arguments: new QueryArguments(new QueryArgument<NonNullGraphType<StringGraphType>> { Name = "id", Description = "id of the blog" }),
+                resolve: context => data.GetBlogByIdAsync(context.GetArgument<string>("id")));
         }
     }
 }
